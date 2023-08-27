4ae03d2f3394f78e8833a7db1bfcbced83b6cbaa
gh1928 <oos1928@gmail.com>
2023-08-27 23:08:32 +0900

    플레이어 이동 / 스테이지 터레인 적용 완료

---
+++ Assets/MyFolder/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 dir;
+
+
+    [SerializeField]
+    private float speed = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.z = Input.GetAxisRaw("Vertical");
+        dir.y = 0;
+        dir = dir.normalized * speed;
+
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+}
