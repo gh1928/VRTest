0c400c343192fa627ad95cdc17c237bf68c0fded
gh1928 <oos1928@gmail.com>
2023-08-29 00:11:24 +0900

    1차기능 완성

--- Assets/MyFolder/Scripts/Manager.cs
+++ Assets/MyFolder/Scripts/Manager.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    public GameObject oiled;
+    public GameObject origin;
+    
+    public MeshRenderer planeRenderer;
+
+    public Material oiledMaterial;
+    public Material originMaterial;
+
+    public Transform playerTransform;
+
+    public Transform groundPivot;
+    public Transform skyPivot;
+    bool atGround = true;
+
+    bool oiledPlain = true;
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            oiled.SetActive(!oiled.activeSelf);
+            origin.SetActive(!origin.activeSelf);
+
+            oiledPlain = !oiledPlain;
+            planeRenderer.material = oiledPlain ? oiledMaterial : originMaterial;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            atGround = !atGround;
+            playerTransform.position = atGround ? groundPivot.position : skyPivot.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
             Application.Quit();
     }
 }
